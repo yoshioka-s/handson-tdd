30a0eab3646ba30f85c5c4555c90f745b877f65c
yoshioka-s <yoshioka63@gmail.com>
2021-01-30 17:27:01 +0900

    fix: cors

--- api/src/app.ts
+++ api/src/app.ts
@@ -5,8 +5,8 @@ import swaggerDocument from '../swagger.json'
 import router from './routes'
 
 const app = express()
+app.use(cors())
 app.use(express.json())
-app.use(cors)
 app.use(router)
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
