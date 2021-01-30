0ab46787eb1034df93d5666ce5674e31bdbaade2
yoshioka-s <yoshioka63@gmail.com>
2021-01-30 15:14:46 +0900

    fix: routes

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,9 +1,12 @@
 import express from 'express'
+import tasks from './tasks'
 
 const router = express.Router()
 
 router.get('/', (_req, res) => {
   return res.send('API is working!!')
 })
+
+router.use('/tasks', tasks)
 
 export default router
