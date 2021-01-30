621782d6f40402403a631e6d2927569892ee38a8
yoshioka-s <yoshioka63@gmail.com>
2021-01-30 15:51:51 +0900

    chore: disable sequelize logging

--- api/db/squelize.ts
+++ api/db/squelize.ts
@@ -1,7 +1,8 @@
 import { Sequelize } from 'sequelize'
 const sequelize = new Sequelize({
   dialect: 'sqlite',
-  storage: './database.sqlite'
+  storage: './database.sqlite',
+  logging: false
 })
 
 export default sequelize
