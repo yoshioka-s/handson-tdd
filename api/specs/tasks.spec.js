de666cee3f47ae35dba3bbcf787f2fa294306be5
yoshioka-s <yoshioka63@gmail.com>
2021-01-30 17:21:18 +0900

    test: specs

--- api/specs/tasks.spec.js
+++ api/specs/tasks.spec.js
@@ -4,16 +4,71 @@ import taskFactory from './factories/task'
 import truncate from './truncate'
 
 describe('tasks endpoints', () => {
+  const api = request(app)
   afterEach(() => {
     return truncate()
   })
-  beforeEach(() => {
-    taskFactory()
-  })
+
   describe('GET /tasks', () => {
-    it('success', async () => {
-      const res = await request(app).get('/tasks')
-      expect(res.statusCode).toBe(200)
+    it('success', done => {
+    })
+
+    it('returns an array of tasks', async () => {
+    })
+  })
+
+  xdescribe('PATCH /tasks/{id}', () => {
+    describe('Given a initialized task', () => {
+      let task
+      beforeEach(async () => {
+        task = await taskFactory()
+      })
+      describe('When send status: started', () => {
+        it('success', done => {
+        })
+        it('updates the status to started', async () => {
+        })
+        it('sets the startedAt', async () => {
+        })
+      })
+    })
+    describe('Given the id does not exit', () => {
+      describe('When send status: started', () => {
+        it('fails', done => {
+        })
+      })
+    })
+  })
+
+  xdescribe('POST /tasks', () => {
+    describe('When name is not empty', () => {
+      const name = 'test'
+      it('success', done => {
+      })
+      it('returns a new record', async () => {
+      })
+    })
+    describe('When name is empty', () => {
+      const name = ''
+      it('fails', done => {
+      })
+    })
+  })
+
+  xdescribe('DELETE /tasks/{id}', () => {
+    describe('When deletes for a existing task', () => {
+      let task
+      beforeEach(async () => {
+        task = await taskFactory()
+      })
+      it('success' , done => {
+      })
+      it('deletes the task', async () => {
+      })
+    })
+    describe('When deletes non existing task', () => {
+      it('fails', done => {
+      })
     })
   })
 })
--- api/src/app.ts
+++ api/src/app.ts
@@ -5,9 +5,9 @@ import swaggerDocument from '../swagger.json'
 import router from './routes'
 
 const app = express()
+app.use(express.json())
 app.use(router)
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.use(cors)
-app.use(express.json())
 
 export default app
